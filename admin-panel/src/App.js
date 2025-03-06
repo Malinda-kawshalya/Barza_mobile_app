f925ec7510ba2aa96666c807e3b50bd1cd9ec5db
Malinda Kawshalya <135861772+Malinda-kawshalya@users.noreply.github.com>
2025-03-06 15:24:04 +0530

    admin panel creation

---
+++ admin-panel/src/App.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
+import Dashboard from "./views/dashboard"; 
+
+function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "testCollection"));
+        querySnapshot.forEach((doc) => {
+          console.log(doc.id, " => ", doc.data());
+        });
+        console.log("Firebase is connected! ✅");
+      } catch (error) {
+        console.error("Error connecting to Firebase ❌", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ admin-panel/src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAHxzDARu1x25Yix1aVuBYcpjyidg4fp3Q",
+    authDomain: "barza-mobile-app.firebaseapp.com",
+    projectId: "barza-mobile-app",
+    storageBucket: "barza-mobile-app.firebasestorage.app",
+    messagingSenderId: "688435936708",
+    appId: "1:688435936708:web:46172d5a2d44468bd03891",
+    measurementId: "G-CVZYZEY745"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
