2a974ed2473e410a6f47598216cec531df44b598
Tharindu Premarathne <tharindumax201@gmail.com>
2025-03-16 01:29:46 +0530

    change UI items

--- barza_app/lib/screens/category_items_screen.dart
+++ barza_app/lib/screens/category_items_screen.dart
@@ -5,13 +5,36 @@ import '../widgets/items.dart'; // Import your Items widget
 class SpecificCategoryItemsScreen extends StatelessWidget {
   final String category;
 
-  SpecificCategoryItemsScreen({required this.category});
+  const SpecificCategoryItemsScreen({required this.category});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 218, 230, 229),
       appBar: AppBar(
-        title: Text(category),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(25),
+            bottomRight: Radius.circular(25),
+          ),
+        ),
+        backgroundColor: const Color(0xFF0C969C),
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          category,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
       ),
       body: Items(
         category: category,
